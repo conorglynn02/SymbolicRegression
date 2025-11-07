da9d80c28aea8257886757cd1024209bba0c0ffb
conorglynn02 <conorglynn02@gmail.com>
2025-11-07 20:08:18 +0000

    Now creates an even population distribution per height.

--- population.py
+++ population.py
@@ -1,17 +1,29 @@
-import random
 from tree.tree import Tree
 
 def make_population(size: int, max_tree_height: int) -> list:
     population = []
-    for _ in range(size):
+
+    base = size // max_tree_height
+    remainder = size % max_tree_height
+
+    heights = []
+    # max_tree_height down to 1 and adds the remainder to the tallest trees
+    for height in range(max_tree_height, 0, -1):
+        count = base + (1 if remainder > 0 else 0)
+        heights.extend([height] * count)
+        if remainder > 0:
+            remainder -= 1
+
+    for i, height in enumerate(heights):
         tree = Tree()
-        random_height = random.randint(1, max_tree_height)
         # half grow and half full
-        if _ % 2 == 0:
+        if i % 2 == 0:
             # grow method
-            tree.grow(max_height=random_height)
+            tree.grow(max_height=height)
+            print(f"Grow of height {height}")
         else:
             # full method
-            tree.full(max_height=random_height)
+            tree.full(max_height=height)
+            print(f"Full of height {height}")
         population.append(tree)
     return population
