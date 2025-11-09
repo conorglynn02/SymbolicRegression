c84b496c3482c45c76c59ed22fbac049157709b3
conorglynn02 <conorglynn02@gmail.com>
2025-11-09 00:17:54 +0000

    Refactor roulette, crossover and mutate.

--- evolve/crossover.py
+++ evolve/crossover.py
@@ -1,7 +1,16 @@
+import random
 import copy
 from tree.tree import Tree
 
-def crossover(mrParent: Tree, msParent: Tree) -> tuple:
+def should_apply_crossover(crossover_rate=1.0) -> bool:
+	return random.random() <= crossover_rate
+
+def choose_parents(parents_pool: list[Tree]) -> tuple[Tree, Tree]:
+	mrParent = random.choice(parents_pool)
+	msParent = random.choice(parents_pool)
+	return mrParent, msParent
+
+def crossover(mrParent: Tree, msParent: Tree) -> tuple[Tree, Tree]:
     awesomeSon  = copy.deepcopy(mrParent)
     greatDaughter  = copy.deepcopy(msParent)
 
@@ -13,3 +22,20 @@ def crossover(mrParent: Tree, msParent: Tree) -> tuple:
     node1.right, node2.right = node2.right, node1.right
 
     return awesomeSon, greatDaughter
+
+def run_crossover(parents_pool: list[Tree], num_children: int, crossover_rate=1.0) -> list[Tree]:
+    children = []
+
+    while len(children) < num_children:
+        mrParent, msParent = choose_parents(parents_pool)
+
+        if should_apply_crossover(crossover_rate):
+            awesomeSon, greatDaughter = crossover(mrParent, msParent)
+        else:
+            awesomeSon, greatDaughter = mrParent, msParent
+
+        children.append(awesomeSon)
+        if len(children) < num_children:
+            children.append(greatDaughter)
+
+    return children
--- evolve/mutation.py
+++ evolve/mutation.py
@@ -1,13 +1,12 @@
 import random
 import copy
 from tree.tree import Tree
-	
-def mutate(individual: Tree, max_height=3, mutation_rate=0.1) -> Tree:
-    if random.random() > mutation_rate:
-        return individual
+
+def should_mutate(mutation_rate=0.1) -> bool:
+    return random.random() <= mutation_rate
+
+def mutate(individual: Tree, max_height=3) -> Tree:
     mutant = copy.deepcopy(individual)
-    # needs to change to be a prob per node to be mutated 
-    # rather than prob of overall tree
     node = mutant.random_node()
     new_subtree = Tree()
     new_subtree.grow(max_height)
@@ -15,3 +14,12 @@ def mutate(individual: Tree, max_height=3, mutation_rate=0.1) -> Tree:
     node.left = new_subtree.root.left
     node.right = new_subtree.root.right
     return mutant
+
+def run_mutations(children: list[Tree], mutation_rate=0.1) -> list[Tree]:
+    mutated = []
+    for child in children:
+        if should_mutate(mutation_rate):
+            mutated.append(mutate(child))
+        else:
+            mutated.append(child)
+    return mutated
